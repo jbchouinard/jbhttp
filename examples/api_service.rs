b3aa3e4741a9a51539c074e2d41a66d56adaed60
Jerome Boisvert-Chouinard <jerome_boisvertchouinard@trendmicro.com>
2021-05-08 18:54:43 -0400

    remove noisy backticks

--- examples/api_service.rs
+++ examples/api_service.rs
@@ -39,9 +39,9 @@ fn main() {
         opt.threads,
         // network socket read/write timeout
         Some(std::time::Duration::from_secs(30)),
-        // An application is a request handler, which accepts `Request<Vec<u8>>`
-        // and returns `Response<Vec<u8>>`. `Request` and `Response` can hold other
-        // payload types, but `Server` can only deal with bytes.
+        // An application is a request handler, which accepts Request<Vec<u8>>
+        // and returns Response<Vec<u8>>. Request and Response can hold other
+        // payload types, but Server can only deal with bytes.
         app(),
     )
     .unwrap();
@@ -49,139 +49,139 @@ fn main() {
     server.serve_forever();
 }
 
-// A request handler must implement the `Handler` trait, which has 4 type parameters:
+// A request handler must implement the Handler trait, which has 4 type parameters:
 //  - Request payload
 //  - Ok response payload
 //  - Err response payload
 //  - Context
 //
-// To handle requests from a server, all payload types must be bytes (`Vec<u8>`).
-// This can be done by implementing a bytes `Handler` directly, or by
-// implementing `Handler` for other types and adding de/serialization filters.
+// To handle requests from a server, all payload types must be bytes (Vec<u8>).
+// This can be done by implementing a bytes Handler directly, or by
+// implementing Handler for other types and adding de/serialization filters.
 //
-// The `Ok` and `Err` response payloads can be the same type, but they
+// The Ok and Err response payloads can be the same type, but they
 // might not be; for example, for Person API it doesn't make sense to return
-// a `Person` on error, we want to return a detailed error, which isn't
-// anything like a `Person`.
+// a Person on error, we want to return a detailed error, which isn't
+// anything like a Person.
 //
-// The context type can be anything, as long as it implements `Default`.
-// Each request is tied to a context; the server creates a new context with `default()`
+// The context type can be anything, as long as it implements Default.
+// Each request is tied to a context; the server creates a new context with default()
 // for each, and request handlers and filters get a mutable ref to it.
-// It can be used to keep track of whatever, for example, the `Context`
+// It can be used to keep track of whatever, for example, the Context
 // type referred to here is implemented later, it is used to keep track of
 // authentication and other details.
 fn app() -> impl Handler<Vec<u8>, Vec<u8>, Vec<u8>, Context> {
-    // The app is built up in layers. `PersonApi` is a REST endpoint which handles
-    // GET, POST and DELETE operations for `Person`. It takes `Request<Person>` and returns
-    // `Result<Response<Person>, Response<ApiError>>`, or `Res<Person, ApiError>` for short.
+    // The app is built up in layers. PersonApi is a REST endpoint which handles
+    // GET, POST and DELETE operations for Person. It takes Request<Person> and returns
+    // Result<Response<Person>, Response<ApiError>>, or Res<Person, ApiError> for short.
     // To use it to for handling requests from Server, we must add de/serialization for:
     //  - Request<Vec<u8>> -> Request<Person>
     //  - Response<Person> -> Response<Vec<u8>>
     //  - Response<ApiError> -> Response<Vec<u8>>
     //
     // Apps are built from a few different kinds of components:
-    //  - A `Handler` handles a request and returns an Ok or Err response.
-    //  - A `Router` dispatches requests to any number of `Handler`s
-    //  - A `RequestFilter` can modify (or replace) requests and pass them forward,
+    //  - A Handler handles a request and returns an Ok or Err response.
+    //  - A Router dispatches requests to any number of Handlers
+    //  - A RequestFilter can modify (or replace) requests and pass them forward,
     //    or pre-empt the request with its own response (typically, in case of error)
-    //  - An `OkFilter` can modify (or replace) `Ok` responses
-    //  - An `ErrFilter` can modify (or replace) `Err` responses
-    //  - A `ResFilter` can modify (or replace) both `Ok` and `Err` responses
+    //  - An OkFilter can modify (or replace) Ok responses
+    //  - An ErrFilter can modify (or replace) Err responses
+    //  - A ResFilter can modify (or replace) both Ok and Err responses
     let person_api = PersonApi
-        // `Api` is a convenience trait to help implement handlers with methods
-        // that map to HTTP verbs - see the implementation of `PersonApi` below.
-        // The `handler` method just constructs a `Handler` for types implementing `Api`.
+        // Api is a convenience trait to help implement handlers with methods
+        // that map to HTTP verbs - see the implementation of PersonApi below.
+        // The handler method just constructs a Handler for types implementing Api.
         .handler()
-        // returns `impl Handler<Person, Person, ApiError, Context>`
+        // returns impl Handler<Person, Person, ApiError, Context>
         //
         // jbhttp includes filters for automatic serialization based on Accept
         // request headers, and deserialization based on Content-Type request headers.
-        // To use them, `Serialize` and `Deserialize` traits must be implemented for
-        // `Person` for the relevant media types. The following methods are
-        // provided for any type implementing `Handler`:
-        //  - `serialized`: Serializes `Ok` response payloads
-        //  - `serialized_error`: Serializes `Err` response payloads
-        //  - `deserialized`:  Deserializes request payloads
-        //  - `serdeserialized`: Deserializes request payloads and serializes
-        //    `Ok` response payloads
+        // To use them, Serialize and Deserialize traits must be implemented for
+        // Person for the relevant media types. The following methods are
+        // provided for any type implementing Handler:
+        //  - serialized: Serializes Ok response payloads
+        //  - serialized_error: Serializes Err response payloads
+        //  - deserialized:  Deserializes request payloads
+        //  - serdeserialized: Deserializes request payloads and serializes
+        //    Ok response payloads
         .serdeserialized()
-        // returns `impl Handler<Vec<u8>, Vec<u8>, ApiError, Context>`
+        // returns impl Handler<Vec<u8>, Vec<u8>, ApiError, Context>
         //
         // Media types are represented as Rust types (unit structs).
-        // To support application/json for `PersonApi`, the following must be implemented:
-        //  - `jbhttp::content::Serialize<ApplicationJson> for Person`
-        // `- `jbhttp::content::Deserialize<Person> for ApplicationJson`
+        // To support application/json for PersonApi, the following must be implemented:
+        //  - jbhttp::content::Serialize<ApplicationJson> for Person
+        // - jbhttp::content::Deserialize<Person> for ApplicationJson
         //
-        // With the `json` crate feature enabled, since our `Person` struct
-        // derives `serde::Serialize` and `serde::Deserialize`, `ApplicationJson`
-        // de/serialization traits are automatically implemented for `Person`.
+        // With the json crate feature enabled, since our Person struct
+        // derives serde::Serialize and serde::Deserialize, ApplicationJson
+        // de/serialization traits are automatically implemented for Person.
         //
-        // `with_media_type` can be called multiple times; order matters,
+        // with_media_type can be called multiple times; order matters,
         // if a client accepts multiple types equally, like Accept: */*, the first
         // acceptable media type is used.
         .with_media_type::<ApplicationJson>();
 
-    // `Router` associates URLs with handlers, and produces 404s response otherwise.
+    // Router associates URLs with handlers, and produces 404s response otherwise.
     Router::new()
-        // `Route`'s have limited pattern matching, see `Router` documentation for details.
-        // For our case here, we want to match an `id` parameter; as of now,
-        // `Router` does not do parameter parsing, so `id` will be a `String`
+        // Route's have limited pattern matching, see Router documentation for details.
+        // For our case here, we want to match an id parameter; as of now,
+        // Router does not do parameter parsing, so id will be a String
         // not, necessarily numeric.
         .with_route("/person/?id", person_api)
         //
-        // `authenticated` is a convenience method on `Handler`'s which constructs
+        // authenticated is a convenience method on Handler's which constructs
         // a specialized request filter for authentication.
         // It takes a callable of type
-        // `Fn(&Request<T>, &mut Context) -> Result<(), AuthError>`
-        // and runs on it on each request; on `Ok(())`, the request is
-        // passed forward; on `Err(e)`, it is pre-empted with a 401 response.
+        // Fn(&Request<T>, &mut Context) -> Result<(), AuthError>
+        // and runs on it on each request; on Ok(()), the request is
+        // passed forward; on Err(e), it is pre-empted with a 401 response.
         //
         // Alternatively, authentication could be implemented with a generic
-        // `RequestFilter` using (`.request_filter(f)`), which takes a callable of type:
-        // `Fn(Request<I>, &mut C) -> Result<Request<FI>, Response<E>>`
-        // where a return of `Ok(some_request)` passes `some_request` forward,
-        // while a return of `Err(some_response)` pre-empts the request and
-        // returns `some_response`.
+        // RequestFilter using (.request_filter(f)), which takes a callable of type:
+        // Fn(Request<I>, &mut C) -> Result<Request<FI>, Response<E>>
+        // where a return of Ok(some_request) passes some_request forward,
+        // while a return of Err(some_response) pre-empts the request and
+        // returns some_response.
         .authenticated(authenticate)
         //
-        // An `ErrorFilter` takes a callable with signature:
+        // An ErrorFilter takes a callable with signature:
         // Fn(Response<FE>, &mut C) -> Response<E>
-        // and applies it (only) to `Err` responses.
+        // and applies it (only) to Err responses.
         //
         // Various parts of the framework can generate error responses;
         // de/serializers can generate 400, 405, 415, etc.; framework-generated
-        // errors always have a `None` payload, since the implementation is
+        // errors always have a None payload, since the implementation is
         // agnostic to payload type.
         //
-        // This function generates default `ApiError` payloads based on status
+        // This function generates default ApiError payloads based on status
         // for empty framework-generated error responses. Note that some errors may
         // occur at a higher level than here, i.e. a 400 or 500 error originating
-        // from `TcpServer` will not go through `generate_error` and will be empty.
+        // from TcpServer will not go through generate_error and will be empty.
         .error_filter(generate_error)
-        // Serialize `ApiError`; again, since `ApiError` derives
-        // `serde::Serialize, `ApplicationJson` is automatically supported.
+        // Serialize ApiError; again, since ApiError derives
+        // serde::Serialize, ApplicationJson is automatically supported.
         .serialized_error()
-        // returns `impl Handler<Vec<u8>, Vec<u8>, Vec<u8>, Context>`
+        // returns impl Handler<Vec<u8>, Vec<u8>, Vec<u8>, Context>
         .with_media_type::<ApplicationJson>()
         //
-        // `res_filter` constructs a `ResFilter` from a callable of type
-        // `Fn(Res<O, E>, &mut Context) -> Res<FO, FE>
-        // `ResFilter` is a combination `OkFilter` and `ErrFilter`, it processes
+        // res_filter constructs a ResFilter from a callable of type
+        // Fn(Res<O, E>, &mut Context) -> Res<FO, FE>
+        // ResFilter is a combination OkFilter and ErrFilter, it processes
         // both types of response; consequently it's the only filter that can change
-        // an `Ok` response into an `Err` response, or vice-versa.
+        // an Ok response into an Err response, or vice-versa.
         //
         // It's used here just to add a request ID to all responses,
-        // whether they are `Ok` or `Err`.
+        // whether they are Ok or Err.
         .res_filter(add_request_id)
 
     // Filters are composable, we could have called the methods here
-    // in different order, but the order of `RequestFilter`s especially
+    // in different order, but the order of RequestFilters especially
     // matters, since they can pre-empt requests.
     //
-    // For example, if we called `authenticated` after `res_filter` instead,
+    // For example, if we called authenticated after res_filter instead,
     // a request that fails authentication would not have the request ID
     // header set, since it would be pre-empted by the authenticator before
-    // reaching the `add_request_id` filter.
+    // reaching the add_request_id filter.
     //
     // Here is a request/response flow diagram of the service, with indications
     // where a request could be pre-empted with error codes.
@@ -203,7 +203,7 @@ fn app() -> impl Handler<Vec<u8>, Vec<u8>, Vec<u8>, Context> {
     // TcpServer
     //   ↑   ↓   ↧ [400, 500]
     //
-    // Handlers routed by `Router` must have matching types,
+    // Handlers routed by Router must have matching types,
     // if we had multiple APIs with heterogenous types, each would have
     // to be de/serialized before routing - here it doesn't matter
     // since there's only one handler.
@@ -281,7 +281,7 @@ struct User(String);
 static REQUEST_COUNTER: AtomicU64 = AtomicU64::new(1);
 
 // Types used as context for Handler must implement Default, each request
-// gets a `default()` context from the server.
+// gets a default() context from the server.
 #[derive(Debug)]
 struct Context {
     request_id: u64,
